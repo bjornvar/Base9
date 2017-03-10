00a5eb2112af67a94bd8b96ce59121183689851b
Bjørn Vårdal <bjornvar@ca.ibm.com>
2017-03-10 10:55:45 -0500

    Add B9RAS struct and initializers

---
+++ b9ras.cpp
@@ -0,0 +1,75 @@
+/*
+ * b9ras.c
+ *
+ *  Created on: Mar 10, 2017
+ *      Author: Bjørn Vårdal
+ */
+
+#include "b9ras.hpp"
+
+static void
+readDDRBlob(B9RAS *b9ras)
+{
+	int rc = 1;
+	int ddrFileLength;
+
+	/* Construct blob file name */
+	char * filename = (char*)malloc(2 + sizeof(DDR_DAT_FILE)); // "./ddr.dat"
+	strcpy(filename, ".");
+	strcat(filename, "/");
+	strcat(filename, DDR_DAT_FILE);
+
+	/* Open the DDR data file and read it into memory - if the file is not there, do not raise an error */
+	FILE *f = fopen(filename, "r");
+	fseek(f, 0L, SEEK_END);
+	ddrFileLength = ftell(f);
+
+	/* Read blob */
+	if ((ddrFileLength > 0) && (ddrFileLength <= 0x7FFFFFFF)) {
+		void *ddrData;
+		ddrData = malloc((UDATA) ddrFileLength);
+		b9ras->ddrData = ddrData;
+
+		if (ddrFileLength != fread(ddrData, 1, ddrFileLength, f)) {
+			printf("Error reading blob\n");
+			free(filename);
+			fclose(f);
+			exit(-1);
+		}
+	}
+	fclose(f);
+	free(filename);
+}
+
+B9RAS *
+initializeJ9RAS(ExecutionContext const *b9context)
+{
+	B9RAS *rasStruct = (B9RAS *)malloc(sizeof(B9RAS));
+
+	memset(rasStruct, 0, sizeof(B9RAS));
+	strcpy((char*)rasStruct->eyecatcher, "J9VMRAS");
+	rasStruct->bitpattern1 = 0xaa55aa55;
+	rasStruct->bitpattern2 = 0xaa55aa55;
+	rasStruct->version = VERSION;
+	rasStruct->length = sizeof(B9RAS);
+	rasStruct->vm = (UDATA)b9context;
+	rasStruct->mainThreadOffset = 0;
+	rasStruct->omrthreadNextOffset = 0;
+	rasStruct->osthreadOffset = 0;
+	rasStruct->idOffset = 0;
+	rasStruct->typedefs = 0;
+	rasStruct->env = 0;
+	rasStruct->buildID = 0;
+	rasStruct->environment = 0;
+	rasStruct->cpus = 1;
+	rasStruct->memory = 1;
+	strcpy((char*)rasStruct->osversion, "unknown");
+	strcpy((char*)rasStruct->osarch, "unknown");
+	strcpy((char*)rasStruct->osname, "unknown");
+	rasStruct->pid = (UDATA)getpid();
+	rasStruct->tid = (UDATA)getpid();
+
+	readDDRBlob(rasStruct);
+
+	return rasStruct;
+}
---
+++ b9ras.hpp
@@ -0,0 +1,62 @@
+/*
+ * b9ras.hpp
+ *
+ *  Created on: Mar 10, 2017
+ *      Author: bjornvar
+ */
+
+#ifndef B9RAS_HPP_
+#define B9RAS_HPP_
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define DDR_DAT_FILE "ddr.dat"
+#define EYE_CATCHER "J9VMRAS"
+#define BIT_PATTERN 0xaa55aa55
+#define VERSION 1
+
+typedef unsigned char U_8;
+typedef unsigned int U_32;
+typedef int I_32;
+typedef unsigned long long U_64;
+typedef unsigned long long UDATA;
+
+class ExecutionContext;
+
+typedef struct B9RAS {
+	U_8 eyecatcher[8];
+	U_32 bitpattern1;
+	U_32 bitpattern2;
+	I_32 version;
+	I_32 length;
+	void* ddrData;
+	UDATA mainThreadOffset;
+	UDATA omrthreadNextOffset;
+	UDATA osthreadOffset;
+	UDATA idOffset;
+	UDATA typedefsLen;
+	UDATA typedefs;
+	UDATA env;
+	UDATA vm;
+	U_64 buildID;
+	U_8 osversion[128];
+	U_8 osarch[16];
+	U_8 osname[48];
+	U_32 cpus;
+	void* environment;
+	U_64 memory;
+	U_8 hostname[256];
+	U_8 ipAddresses[256];
+	UDATA pid;
+	UDATA tid;
+} B9RAS;
+
+B9RAS *initializeJ9RAS(ExecutionContext const *b9context);
+
+#endif /* B9RAS_HPP_ */
